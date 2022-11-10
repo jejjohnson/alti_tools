5fca2c0f85a20fc4540511e14eddfd15785ab0ce
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2022-11-10 18:17:11 +0100

    fixed some downloads.

--- alti_tools/_src/data/ssh.py
+++ alti_tools/_src/data/ssh.py
@@ -12,7 +12,9 @@ URL_test = "https://raw.githubusercontent.com/adekunleoajayi/powerspec/master/po
 URL_OSSE_OBS_BOTH_SSH = "https://s3.us-east-1.wasabisys.com/melody/osse_data/data/gridded_data_swot_wocorr/dataset_nadir_0d_swot.nc"
 URL_OSSE_OBS_NADIR_SSH = "https://s3.us-east-1.wasabisys.com/melody/osse_data/data/gridded_data_swot_wocorr/dataset_nadir_0d.nc"
 URL_OSSE_OBS_SWOT_SSH = "https://s3.us-east-1.wasabisys.com/melody/osse_data/data/gridded_data_swot_wocorr/dataset_swot.nc"
-URL_OSSE_DUACS_SSH = "https://s3.us-east-1.wasabisys.com/melody/osse_data/oi/ssh_NATL60_swot_4nadir.nc"
+URL_OSSE_DUACS_SSH = (
+    "https://s3.us-east-1.wasabisys.com/melody/osse_data/oi/ssh_NATL60_swot_4nadir.nc"
+)
 URL_OSSE_NATL60_SSH = "https://s3.us-east-1.wasabisys.com/melody/osse_data/ref/NATL60-CJM165_GULFSTREAM_ssh_y2013.1y.nc"
 URL_OSSE_NATL60_SST = "https://s3.us-east-1.wasabisys.com/melody/osse_data/ref/NATL60-CJM165_GULFSTREAM_sst_y2013.1y.nc"
 URL_OSSE_NATL60_SSC = "https://s3.us-east-1.wasabisys.com/melody/osse_data/ref/NATL60-CJM165_GULFSTREAM_sss_y2013.1y.nc"
@@ -25,7 +27,11 @@ URL_QG_IDEAL_COMPLEX_256 = "https://ige-meom-opendap.univ-grenoble-alpes.fr//thr
 URL_QG_IDEAL_COMPLEX_128 = "https://ige-meom-opendap.univ-grenoble-alpes.fr//thredds/fileServer/meomopendap/extract/dc2022b_q/trial_res/qgsim_complex_128x128_L1_D1.nc"
 URL_QG_IDEAL_SIMPLE_128_1000s = "https://ige-meom-opendap.univ-grenoble-alpes.fr//thredds/fileServer/meomopendap/extract/dc2022b_q/qgsim_simple_128x128_dt1000.nc"
 
-def get_ssh_url(dataset: str="natl60")->str:
+# QG SSH SIMULATIONS
+URL_QG_SSH = "https://ige-meom-opendap.univ-grenoble-alpes.fr/thredds/fileServer/meomopendap/extract/ocean-data-challenges/2022b_SSH_QG_mapping/dc_qg_eval.tar.gz"
+
+
+def get_ssh_url(dataset: str = "natl60") -> str:
     if dataset.lower() == "natl60":
         return URL_NATL60
     elif dataset.lower() == "4dvarnet":
@@ -40,11 +46,15 @@ def get_ssh_url(dataset: str="natl60")->str:
         raise ValueError("Unrecognized dataset")
 
 
-def download_ssh_toy(dataset: str="natl60", *args, **kwargs) -> None:    
+def download_ssh_toy(dataset: str = "natl60", *args, **kwargs) -> None:
     url = get_ssh_url(dataset)
-    runcmd(f"wget -nc {url}",  *args, **kwargs)
+    runcmd(f"wget -nc {url}", *args, **kwargs)
     return None
 
-def download_osse_demo(dataset: str="", *args,) -> None:
+
+def download_osse_demo(
+    dataset: str = "",
+    *args,
+) -> None:
 
     return None
